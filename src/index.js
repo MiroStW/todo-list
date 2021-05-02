219adf9eed94c1b4fff5572ef429138c90c682c1
Miro <miro@miro-wilms.de>
2021-05-02 13:34:01 +0200

    put initial page in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-import { initialPage } from "./layout/initial_page";
+import showProjectList from './layout/projects_view';
+import { showTodoList } from './layout/todos_view';
 
-// separate logic from DOM
-// store in localstorage
-// use date-fns to handle duedate
+const container = document.querySelector('#content');
+
+// header
+const header = document.createElement('div');
+header.classList.add('header');
+header.textContent = 'Todo system';
+container.appendChild(header);
+
+// project list
+const projectArea = document.createElement('div');
+projectArea.classList.add('projectarea');
+container.appendChild(projectArea);
+
+// Todo list
+const todoArea = document.createElement('div');
+todoArea.classList.add('todoarea');
+container.appendChild(todoArea);
+
+showProjectList();
+showTodoList('showProject');
