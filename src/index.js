469e6baadb4cb639e86dfe4cfa793d61dd4e3a06
Miro <miro@miro-wilms.de>
2022-02-23 20:46:10 +0100

    updated config

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 import showProjectList from "./layout/projects_view";
 import { showTodoList } from "./layout/todos_view";
+import styles from "./style.module.css";
 
-const container = document.querySelector("#content");
+const root = document.querySelector("#root");
 
 // header
 const header = document.createElement("div");
-header.classList.add("header");
+header.classList.add(styles.header);
 header.textContent = "Todo system";
-container.appendChild(header);
+root.appendChild(header);
 
 // project list
 const projectArea = document.createElement("div");
-projectArea.classList.add("projectarea");
-container.appendChild(projectArea);
+projectArea.classList.add(styles.projectarea);
+root.appendChild(projectArea);
 
 // Todo list
 const todoArea = document.createElement("div");
-todoArea.classList.add("todoarea");
-container.appendChild(todoArea);
+todoArea.classList.add(styles.todoarea);
+root.appendChild(todoArea);
 
 showProjectList();
 showTodoList("showProject");
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,80 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: ["./src/index.js", "./dist/style.css"],
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].js", // filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    // publicPath: "/todo-list/",
+  },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        test: /^((?!\.module).)*css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: false,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.module.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                localIdentName: "[path][name]__[local]",
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.tsx?$/,
+        include: path.resolve("src"),
+        use: ["babel-loader", "ts-loader"],
       },
     ],
   },
-  mode: "development",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+  devServer: {
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    modules: ["src", "node_modules"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Todo-list app",
+      template: "./src/index.html",
+    }),
+    new HtmlWebpackPlugin({
+      title: "Todo-list app - 404",
+      template: "./src/404.html",
+      filename: "404.html",
+    }),
+  ],
+  devtool: "eval-source-map",
+  optimization: {
+    moduleIds: "deterministic",
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
   },
 };
