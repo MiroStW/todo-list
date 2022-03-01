93a6b4fdc86e548367cdec8685dbf0b1809451d9
Miro <miro@miro-wilms.de>
2022-03-01 22:08:42 +0100

    separated firebaseUI-config

---
+++ src/firebaseUI-config.js
@@ -0,0 +1,40 @@
+import {
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  EmailAuthProvider,
+} from "firebase/auth";
+
+const uiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult(authResult, redirectUrl) {
+      // console.log("signInSuccessWithAuthResult called, user:");
+      // console.log(authResult.user);
+      // User successfully signed in.
+      // Return type determines whether we continue the redirect automatically
+      // or whether we leave that to developer to handle.
+
+      return false;
+    },
+    uiShown() {
+      // The widget is rendered.
+      // Hide the loader.
+      document.getElementById("loader").style.display = "none";
+    },
+  },
+  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+  signInFlow: "popup",
+  signInSuccessUrl: "/",
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    GoogleAuthProvider.PROVIDER_ID,
+    GithubAuthProvider.PROVIDER_ID,
+    EmailAuthProvider.PROVIDER_ID,
+    // firebase.auth.AnonymousAuthProvider.PROVIDER_ID,
+  ],
+  // Terms of service url.
+  // tosUrl: "<your-tos-url>",
+  // Privacy policy url.
+  // privacyPolicyUrl: "<your-privacy-policy-url>",
+};
+
+export default uiConfig;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  onAuthStateChanged,
-  GoogleAuthProvider,
-  GithubAuthProvider,
-  EmailAuthProvider,
-} from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import * as firebaseui from "firebaseui";
 import "firebaseui/dist/firebaseui.css";
 import firebaseConfig from "./firebase-config";
+import uiConfig from "./firebaseUI-config";
 import showProjectList from "./layout/projects_view";
 import { showTodoList } from "./layout/todos_view";
 import styles from "./style.module.css";
@@ -47,53 +42,12 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const fbAuthUi = new firebaseui.auth.AuthUI(auth);
 
-const uiConfig = {
-  callbacks: {
-    signInSuccessWithAuthResult(authResult, redirectUrl) {
-      console.log("signInSuccessWithAuthResult called, user:");
-      console.log(authResult.user);
-      // User successfully signed in.
-      // Return type determines whether we continue the redirect automatically
-      // or whether we leave that to developer to handle.
-      if (authResult.user) {
-        authArea.setAttribute("hidden", "true");
-        projectArea.removeAttribute("hidden");
-        todoArea.removeAttribute("hidden");
-        showProjectList();
-        showTodoList("showProject");
-      }
-
-      return false;
-    },
-    uiShown() {
-      // The widget is rendered.
-      // Hide the loader.
-      document.getElementById("loader").style.display = "none";
-    },
-  },
-  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-  signInFlow: "popup",
-  signInSuccessUrl: "/",
-  signInOptions: [
-    // Leave the lines as is for the providers you want to offer your users.
-    GoogleAuthProvider.PROVIDER_ID,
-    GithubAuthProvider.PROVIDER_ID,
-    EmailAuthProvider.PROVIDER_ID,
-    // firebase.auth.AnonymousAuthProvider.PROVIDER_ID,
-  ],
-  // Terms of service url.
-  // tosUrl: "<your-tos-url>",
-  // Privacy policy url.
-  // privacyPolicyUrl: "<your-privacy-policy-url>",
-};
-
-function initApp() {
-  console.log("initApp called");
-}
 onAuthStateChanged(auth, (user) => {
   if (user) {
     console.log(user);
+    console.log(auth);
     authArea.setAttribute("hidden", "true");
+    loader.setAttribute("hidden", "true");
     projectArea.removeAttribute("hidden");
     todoArea.removeAttribute("hidden");
 
@@ -104,11 +58,5 @@ onAuthStateChanged(auth, (user) => {
     authArea.removeAttribute("hidden");
     projectArea.setAttribute("hidden", "true");
     todoArea.setAttribute("hidden", "true");
-    console.log("no user:");
-    console.log(user);
   }
-});
-
-window.addEventListener("load", () => {
-  initApp();
 });
