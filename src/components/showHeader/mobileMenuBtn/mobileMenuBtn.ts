014296a6e2d4cfb7345737a17e1f1f422667e87e
Miro <miro@miro-wilms.de>
2022-05-26 16:47:37 +0200

    added mobileMenu open/close functionality

--- src/components/showHeader/mobileMenuBtn/mobileMenuBtn.ts
+++ src/components/showHeader/mobileMenuBtn/mobileMenuBtn.ts
@@ -1,25 +1,24 @@
 import addIcon from "components/helpers/buttons/addIcon";
 import styles from "./mobileMenuBtn.module.css";
 import headerStyles from "../header.module.css";
-
-const openSideBar = () => {
-  console.log("works");
-  // 1. show projects bar (ideally transition)
-  // 2. hide todo list (maybe just hidden underneath)
-  // 3. change menu btn
-};
+import projectStyles from "components/showProjects/showProjects.module.css";
 
 const mobileMenuBtn = () => {
+  const projectArea = document.querySelector(`.${projectStyles.projectarea}`)!;
   const header = document.querySelector(`.${headerStyles.header}`)!;
 
   const btn = document.createElement("div");
   btn.classList.add(styles.mobileMenuBtn);
   header.appendChild(btn);
 
-  addIcon(btn, "menu", "filled", 24);
+  const openMenuIcon = addIcon(btn, "menu", "filled", 24);
+  const closeMenuIcon = addIcon(btn, "close", "filled", 24);
+  closeMenuIcon.classList.add(styles.hideIcon);
 
   btn.addEventListener("click", () => {
-    openSideBar();
+    projectArea.classList.toggle(styles.open);
+    openMenuIcon.classList.toggle(styles.hideIcon);
+    closeMenuIcon.classList.toggle(styles.hideIcon);
   });
 };
 
