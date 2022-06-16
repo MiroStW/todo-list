f55c36eca9f9c034f10281664e8e5c7d07016015
Miro <miro@miro-wilms.de>
2022-06-16 15:31:14 +0200

    simplified structure

--- src/components/showApp.ts
+++ src/components/showApp.ts
@@ -1,9 +1,10 @@
-import showHeader from "./showHeader/header";
+import showHeader from "./showHeader/showHeader";
 import todoStyles from "./showTodos/showTodos.module.css";
 import projectStyles from "./showProjects/showProjects.module.css";
 import styles from "style.module.css";
 import showLoader from "components/helpers/loader/loader";
 import { showSnackbar } from "components/helpers/snackbar/snackbar";
+import { showProjects } from "./showProjects/showProjects";
 
 const root = document.querySelector("#root")!;
 const main = document.createElement("div");
@@ -12,10 +13,7 @@ const todoArea = document.createElement("div");
 const authArea = document.createElement("div");
 const loader = showLoader();
 
-const showApp = () => {
-  // header
-  showHeader();
-
+const showContent = () => {
   main.classList.add(styles.main);
   root.appendChild(main);
 
@@ -23,6 +21,8 @@ const showApp = () => {
   projectArea.classList.add(projectStyles.projectarea);
   projectArea.setAttribute("hidden", "true");
   main.appendChild(projectArea);
+
+  showProjects(projectArea);
 
   // Todo list
   todoArea.classList.add(todoStyles.todoarea);
@@ -32,6 +32,14 @@ const showApp = () => {
   // sign-in flow
   authArea.setAttribute("id", "firebaseui-auth-container");
   main.appendChild(authArea);
+};
+
+const showApp = () => {
+  // header
+  showHeader();
+
+  // show authArea, projectArea, todoArea
+  showContent();
 
   // show alerts
   showSnackbar();
@@ -43,6 +51,7 @@ const changeUI = (
 ) => {
   switch (element) {
     case "projectArea":
+      // toggleVisibility()?
       action === "show"
         ? projectArea.removeAttribute("hidden")
         : projectArea.setAttribute("hidden", "true");
