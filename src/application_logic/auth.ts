e3f0254e9407e01642794a1b4214a35e0527e296
Miro <miro@miro-wilms.de>
2022-07-06 20:40:15 +0200

    cleanup

--- src/application_logic/auth.ts
+++ src/application_logic/auth.ts
@@ -8,26 +8,24 @@ import * as firebaseui from "firebaseui";
 import "firebaseui/dist/firebaseui.css";
 import uiConfig from "firebaseUI-config.js";
 
-interface authUserProps {
-  onUserLogin: () => void;
-  onUserLoggedOut: () => void;
-}
-
 const auth = getAuth();
 // comment out this line to switch to production db
 // connectAuthEmulator(auth, "http://localhost:9099");
 const fbAuthUi = new firebaseui.auth.AuthUI(auth);
+
+interface authUserProps {
+  onUserLogin: () => void;
+  onUserLoggedOut: () => void;
+}
 
 const authUser = ({ onUserLogin, onUserLoggedOut }: authUserProps) => {
   onAuthStateChanged(auth, (user) => {
     if (user) {
       // console.log(user);
       // console.log(auth);
-
       onUserLogin();
     } else {
       fbAuthUi.start("#firebaseui-auth-container", uiConfig);
-
       onUserLoggedOut();
     }
   });
