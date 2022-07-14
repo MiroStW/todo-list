e1743ccb850f5cc97795ffe62de15155f07c97af
Miro <miro@miro-wilms.de>
2022-07-14 14:41:23 +0200

    added missing eslint ts parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,18 @@
 module.exports = {
+  parser: "@typescript-eslint/parser",
   env: {
     browser: true,
     es2021: true,
   },
-  extends: ["airbnb-base", "prettier"],
+  extends: [
+    "airbnb-base",
+    "prettier",
+    "eslint:recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
@@ -11,6 +20,9 @@ module.exports = {
   rules: {
     "no-unused-expressions": ["error", { allowTernary: true }],
     "no-param-reassign": ["error", { props: false }],
+    "arrow-body-style": "off",
+    "no-else-return": "off",
     "import/prefer-default-export": "off",
+    "import/extensions": "off",
   },
 };
